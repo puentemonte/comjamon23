b213d49edbf319e25db178c016d12e6debae07e3
frmolla <frmolla@ucm.es>
2023-03-10 11:39:02 +0100

    GameManager + .gitignore

---
+++ comjamon23/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region parameters
+
+    #endregion
+
+    #region properties
+    static private GameManager _instance;
+
+    static public GameManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+    #endregion
+
+    #region references
+
+    #endregion
+
+    #region methods
+
+    #endregion  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
